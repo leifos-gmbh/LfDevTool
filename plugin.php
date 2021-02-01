3fea9192b8c674298370dbee522e85255a7d22c1
Alexander Killing <killing@leifos.de>
2021-02-01 17:23:21 +0100

    updated version

--- plugin.php
+++ plugin.php
@@ -6,12 +6,12 @@
 $id = "lfdevtool";
 
 // code version; must be changed for all code changes
-$version = "0.0.1";
+$version = "0.0.2";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "6.0";
-$ilias_max_version = "6.99";
+$ilias_max_version = "8.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Alexander Killing";
