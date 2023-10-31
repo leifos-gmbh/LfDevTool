e3ee29a6017e5be8757013abaea09cb82c610cd9
Alexander Killing <killing@leifos.de>
2023-10-31 08:19:43 +0100

    updated version

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@ $version = "0.0.3";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "6.0";
-$ilias_max_version = "9.99";
+$ilias_max_version = "10.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Alexander Killing";
